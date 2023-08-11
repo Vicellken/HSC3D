ccd37c9959082b35bc085916ad405ae907404439
Yifei Gu <guyf0601@connect.hku.hk>
2023-08-11 19:22:21 +0800

    review version

--- README.md
+++ README.md
@@ -2,9 +2,20 @@
 
 HSC3D: a Python package to quantify three-dimensional habitat structural complexity
 
+## Setup environment
+
+Install Anaconda or miniconda to your device. See more information at [this link](https://docs.conda.io/projects/conda/en/latest/user-guide/install/download.html).
+
+```
+# choose your own virtual environment name
+conda create -n hsc3d_test python=3.10 -y
+
+conda activate hsc3d_test
+```
+
 ## Installation
 
-``pip install hsc3d`` to install the package or clone the code and install through ``pip intall -e .``
+``pip install HSC3D``
 
 ## Usage
 
--- setup.py
+++ setup.py
@@ -7,22 +7,16 @@ PARENT = FILE.parent  # root directory
 README = (PARENT / 'README.md').read_text(encoding='utf-8')
 REQUIREMENTS = [f'{x.name}{x.specifier}' for x in pkg.parse_requirements((PARENT / 'requirements.txt').read_text())]
 
-VERSION = '0.1.0'
+VERSION = '0.2.0'
 
 setup(
     name='HSC3D',  # name of pypi package
     version=VERSION,  # version of pypi package
-    python_requires='>=3.6',
+    python_requires='>=3.10',
     license='MIT',
     description=('HSC3D: a Python package to quantify three-dimensional habitat structural complexity'),
     long_description=README,
     long_description_content_type='text/markdown',
-    url='https://github.com/Vicellken/HSC3D',
-    project_urls={
-        'Bug Reports': 'https://github.com/Vicellken/HSC3D/issues',
-        'Source': 'https://github.com/Vicellken/HSC3D'},
-    author='Hu, Jiamian; Gu, Yi-Fei',
-    author_email='guyf0601@connect.hku.hk',
     packages=find_packages(),  # required
     include_package_data=True,
     install_requires=REQUIREMENTS,
