5af4ee56bc664b9950e7cbabe853599c8d38e4dd
Yifei Gu <guyf0601@connect.hku.hk>
2024-01-17 15:01:06 +0800

    fix 0.2.0 docs

--- setup.py
+++ setup.py
@@ -4,20 +4,26 @@ from setuptools import find_packages, setup
 
 FILE = Path(__file__).resolve()
 PARENT = FILE.parent  # root directory
-README = (PARENT / 'README.md').read_text(encoding='utf-8')
-REQUIREMENTS = [f'{x.name}{x.specifier}' for x in pkg.parse_requirements((PARENT / 'requirements.txt').read_text())]
+README = (PARENT / "README.md").read_text(encoding="utf-8")
+REQUIREMENTS = [
+    f"{x.name}{x.specifier}"
+    for x in pkg.parse_requirements((PARENT / "requirements.txt").read_text())
+]
 
-VERSION = '0.2.0'
+VERSION = "0.2.2"
 
 setup(
-    name='HSC3D',  # name of pypi package
+    name="HSC3D",  # name of pypi package
     version=VERSION,  # version of pypi package
-    python_requires='>=3.10',
-    license='MIT',
-    description=('HSC3D: a Python package to quantify three-dimensional habitat structural complexity'),
+    python_requires=">=3.10",
+    license="MIT",
+    description=(
+        "HSC3D: a Python package to quantify three-dimensional habitat structural complexity"
+    ),
     long_description=README,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     packages=find_packages(),  # required
     include_package_data=True,
     install_requires=REQUIREMENTS,
-    keywords='3D point clouds,')
+    keywords="3D point clouds, structural complexity",
+)
