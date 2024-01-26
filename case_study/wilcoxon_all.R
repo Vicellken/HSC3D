6f539b4e0ab1d94b610c1bdd467d024a6be5c410
Yifei Gu <guyf0601@connect.hku.hk>
2024-01-26 16:23:15 +0800

    Update wilcoxon_all.R

--- case_study/wilcoxon_all.R
+++ case_study/wilcoxon_all.R
@@ -3,119 +3,136 @@ library(ggplot2)
 library(ggpubr)
 library(gridExtra)
 
-a <- ggboxplot(df, x = "object", y = "rough",
-               xlab = FALSE,
-               ylab = "Structural roughness",
-               palette = "npg", group = "object",
-               color = "object", shape = "object",
-               legend = "none", add = "jitter",
-               add.params = list(size = 3)
-) + labs(title = "a") + 
-  font("ylab", size = 20) + 
-  font("title", size = 23, face = "bold") + 
-  theme(axis.text = element_text(size = 21)) + 
+a <- ggboxplot(df,
+  x = "object", y = "rough",
+  xlab = FALSE,
+  ylab = "Structural roughness",
+  palette = "npg", group = "object",
+  color = "object", shape = "object",
+  legend = "none", add = "jitter",
+  add.params = list(size = 3)
+) + labs(title = "a. Structural roughness") +
+  font("ylab", size = 20) +
+  font("title", size = 20, face = "bold") +
+  theme(axis.text = element_text(size = 20)) +
   stat_compare_means(
     method = "wilcox.test",
     paired = FALSE,
     ref.group = NULL,
     label.x = 1.5,
-    size = 6
-)  
+    label.y.npc = "top",
+    size = 6,
+    label = "p.signif"
+  )
 
-b <- ggboxplot(df, x = "object", y = "log_curv",
-               xlab = FALSE,
-               ylab = "log(Gaussian curvature)",
-               palette = "npg", group = "object",
-               color = "object", shape = "object",
-               legend = "none", add = "jitter",
-               add.params = list(size = 3)
-) + labs(title = "b") + 
-  font("ylab", size = 20) + 
-  font("title", size = 23, face = "bold") + 
-  theme(axis.text = element_text(size = 21)) + 
+b <- ggboxplot(df,
+  x = "object", y = "log_curv",
+  xlab = FALSE,
+  ylab = "log(Gaussian curvature)",
+  palette = "npg", group = "object",
+  color = "object", shape = "object",
+  legend = "none", add = "jitter",
+  add.params = list(size = 3)
+) + labs(title = "b. Gaussian curvature") +
+  font("ylab", size = 20) +
+  font("title", size = 20, face = "bold") +
+  theme(axis.text = element_text(size = 20)) +
   stat_compare_means(
     method = "wilcox.test",
     paired = FALSE,
     ref.group = NULL,
     label.x = 1.5,
-    size = 6
-)
+    label.y.npc = "top",
+    size = 6,
+    label = "p.signif"
+  )
 
-c <- ggboxplot(df, x = "object", y = "log_convex",
-               xlab = FALSE,
-               ylab = "log(Convex hull)",
-               palette = "npg", group = "object",
-               color = "object", shape = "object",
-               legend = "none", add = "jitter",
-               add.params = list(size = 3)
-) + labs(title = "c") + 
-  font("ylab", size = 20) + 
-  font("title", size = 23, face = "bold") + 
-  theme(axis.text = element_text(size = 21)) + 
+c <- ggboxplot(df,
+  x = "object", y = "log_convex",
+  xlab = FALSE,
+  ylab = "log(Convex hull)",
+  palette = "npg", group = "object",
+  color = "object", shape = "object",
+  legend = "none", add = "jitter",
+  add.params = list(size = 3)
+) + labs(title = "c. Convex hull") +
+  font("ylab", size = 20) +
+  font("title", size = 20, face = "bold") +
+  theme(axis.text = element_text(size = 20)) +
   stat_compare_means(
     method = "wilcox.test",
     paired = FALSE,
     ref.group = NULL,
     label.x = 1.5,
-    size = 6
-)
+    label.y.npc = "top",
+    size = 6,
+    label = "p.signif"
+  )
 
-d <- ggboxplot(df, x = "object", y = "log_alpha",
-               xlab = FALSE,
-               ylab = "log(Aplha shape)",
-               palette = "npg", group = "object",
-               color = "object", shape = "object",
-               legend = "none", add = "jitter",
-               add.params = list(size = 3)
-) + labs(title = "d") + 
-  font("ylab", size = 20) + 
-  font("title", size = 23, face = "bold") + 
-  theme(axis.text = element_text(size = 21)) + 
+d <- ggboxplot(df,
+  x = "object", y = "log_alpha",
+  xlab = FALSE,
+  ylab = "log(Aplha shape)",
+  palette = "npg", group = "object",
+  color = "object", shape = "object",
+  legend = "none", add = "jitter",
+  add.params = list(size = 3)
+) + labs(title = "d. Aplha shape") +
+  font("ylab", size = 20) +
+  font("title", size = 20, face = "bold") +
+  theme(axis.text = element_text(size = 20)) +
   stat_compare_means(
     method = "wilcox.test",
     paired = FALSE,
     ref.group = NULL,
     label.x = 1.5,
-    size = 6
-)
+    label.y.npc = "top",
+    size = 6,
+    label = "p.signif"
+  )
 
-e <- ggboxplot(df, x = "object", y = "log_entropy",
-               xlab = FALSE,
-               ylab = "log(Shannon entropy)",
-               palette = "npg", group = "object",
-               color = "object", shape = "object",
-               legend = "none", add = "jitter",
-               add.params = list(size = 3)
-) + labs(title = "e") + 
-  font("ylab", size = 20) + 
-  font("title", size = 23, face = "bold") + 
-  theme(axis.text = element_text(size = 21)) + 
+e <- ggboxplot(df,
+  x = "object", y = "log_entropy",
+  xlab = FALSE,
+  ylab = "log(Shannon entropy)",
+  palette = "npg", group = "object",
+  color = "object", shape = "object",
+  legend = "none", add = "jitter",
+  add.params = list(size = 3)
+) + labs(title = "e. Shannon entropy") +
+  font("ylab", size = 20) +
+  font("title", size = 20, face = "bold") +
+  theme(axis.text = element_text(size = 20)) +
   stat_compare_means(
     method = "wilcox.test",
     paired = FALSE,
     ref.group = NULL,
     label.x = 1.5,
-    size = 6
-)
+    label.y.npc = "top",
+    size = 6,
+    label = "p.signif"
+  )
 
-f <- ggboxplot(df, x = "object", y = "log_gmm",
-               xlab = FALSE,
-               ylab = "log(GMM)",
-               palette = "npg", group = "object",
-               color = "object", shape = "object",
-               legend = "none", add = "jitter",
-               add.params = list(size = 3)    
-) + labs(title = "f") + 
-  font("ylab", size = 20) + 
-  font("title", size = 23, face = "bold") + 
-  theme(axis.text = element_text(size = 21)) + 
+f <- ggboxplot(df,
+  x = "object", y = "log_gmm",
+  xlab = FALSE,
+  ylab = "log(GMM)",
+  palette = "npg", group = "object",
+  color = "object", shape = "object",
+  legend = "none", add = "jitter",
+  add.params = list(size = 3)
+) + labs(title = "f. GMM") +
+  font("ylab", size = 20) +
+  font("title", size = 20, face = "bold") +
+  theme(axis.text = element_text(size = 20)) +
   stat_compare_means(
     method = "wilcox.test",
     paired = FALSE,
     ref.group = NULL,
     label.x = 1.5,
-    size = 6
-)
+    label.y.npc = "top",
+    size = 6,
+    label = "p.signif"
+  )
 
 grid.arrange(a, b, c, d, e, f, nrow = 2, ncol = 3)
-
